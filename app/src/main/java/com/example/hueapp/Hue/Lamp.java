a52f72be2b79535629f07c549460b82998ae2a57
Lavictus <mark2baaij@gmail.com>
2019-11-21 09:30:56 +0100

    Added lamp related classes

--- app/src/main/java/com/example/hueapp/Hue/Lamp.java
+++ app/src/main/java/com/example/hueapp/Hue/Lamp.java
@@ -1,4 +1,16 @@
 package com.example.hueapp.Hue;
 
 public class Lamp {
+
+    LampState lampState;
+    String name;
+    String uniqueID;
+
+    private String TAG = "LAMP";
+
+    public Lamp(LampState lampState, String name, String uniqueID) {
+        this.lampState = lampState;
+        this.name = name;
+        this.uniqueID = uniqueID;
+    }
 }
