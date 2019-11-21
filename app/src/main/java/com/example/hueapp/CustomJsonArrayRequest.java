2d60a08b658f09566617ee1f2bc7b38d25ae36a7
Luke Taylor <luketaylor001@gmail.com>
2019-11-21 16:56:34 +0100

    added JSONrequest class

--- app/src/main/java/com/example/hueapp/CustomJsonArrayRequest.java
+++ app/src/main/java/com/example/hueapp/CustomJsonArrayRequest.java
@@ -1,6 +1,43 @@
 package com.example.hueapp;
 
-public class CustomJsonArrayRequest {
+import com.android.volley.NetworkResponse;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.JsonRequest;
+
+import org.jetbrains.annotations.Nullable;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
+import org.json.JSONArray;
+
+import java.io.UnsupportedEncodingException;
+
+public class CustomJsonArrayRequest extends JsonRequest<JSONArray> {
+
+
+    public CustomJsonArrayRequest(int method, String url, @Nullable JSONObject requestBody, Response.Listener<JSONArray> listener, Response.ErrorListener errorListener) {
+        super(  method,
+                url,
+                requestBody.toString(),
+                listener,
+                errorListener);
+    }
+
+    @Override
+    protected Response<JSONArray> parseNetworkResponse(NetworkResponse response) {
+        Response parsingResult;
+        try {
+            String responseInText = new String(response.data,
+                    HttpHeaderParser.parseCharset(response.headers, PROTOCOL_CHARSET));
+            JSONArray returnArray = new JSONArray(responseInText);
+            parsingResult = Response.success(returnArray, HttpHeaderParser.parseCacheHeaders(response));
+        } catch (UnsupportedEncodingException | JSONException e) {
+            parsingResult = Response.error(new VolleyError("Returned info is not a JsonArray"));
+        }
+        return parsingResult;
+    }
 }
