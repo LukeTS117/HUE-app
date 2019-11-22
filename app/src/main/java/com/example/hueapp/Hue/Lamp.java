0fc6b43a39ee01eac4158444aec150427f3642ed
Lavictus <mark2baaij@gmail.com>
2019-11-22 15:54:48 +0100

    WIP

--- app/src/main/java/com/example/hueapp/Hue/Lamp.java
+++ app/src/main/java/com/example/hueapp/Hue/Lamp.java
@@ -1,16 +1,26 @@
 package com.example.hueapp.Hue;
 
+import android.util.Log;
+
 public class Lamp {
 
     LampState lampState;
     String name;
     String uniqueID;
 
+    boolean selected;
     private String TAG = "LAMP";
 
     public Lamp(LampState lampState, String name, String uniqueID) {
         this.lampState = lampState;
         this.name = name;
         this.uniqueID = uniqueID;
+        this.selected = false;
+        Log.i(TAG, "Lamp " + uniqueID + " created");
+    }
+
+    public void Selected(){
+        this.selected = !this.selected;
+        Log.i(TAG, "Lamp " + uniqueID + " selected");
     }
 }
